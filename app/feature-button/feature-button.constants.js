e38aa181e7ba47aec4c5e3f406adcb66a45c2206
Orsolya Kardos <orsolya.kardos@yahoo.com>
2016-11-06 15:35:20 +0100

    Added svg icons and positioned them correctly, modified desktop design

--- app/feature-button/feature-button.constants.js
+++ app/feature-button/feature-button.constants.js
@@ -3,28 +3,28 @@
 angular.module('app.feature-button')
 .constant('BUTTONCONFIG', {
   'rotate-left' : {
-    imgURL: 'images/rotate_left.png'
+    imgURL: 'images/rotate-left.svg'
   },
   'rotate-right' : {
-    imgURL: 'images/rotate_right.png'
+    imgURL: 'images/rotate-right.svg'
   },
   'rotate-180' : {
-    imgURL: 'images/rotate_180.png'
+    imgURL: 'images/rotate-180.svg'
   },
   'flip-horizontal' : {
-    imgURL: 'images/flip-horizontal.png'
+    imgURL: 'images/flip-horizontal.svg'
   },
   'flip-vertical' : {
-    imgURL: 'images/flip-vertical.png'
+    imgURL: 'images/flip-vertical.svg'
   },
   'grayscale' : {
-    imgURL: 'images/grayscale.png'
+    imgURL: 'images/grayscale.svg'
   },
   'cancel' : {
-    imgURL: 'images/cancel.png'
+    imgURL: 'images/cancel.svg'
   },
   'checkmark' : {
-    imgURL: 'images/checkmark.png'
+    imgURL: 'images/checkmark.svg'
   }
 });
 })();
