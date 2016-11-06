99afd0d4c7c33b558d472c6e6120126495b18a41
Szegedi Dávid <dszegedi@yandex.com>
2016-11-06 18:31:21 +0100

    Event listener calling limitation
    Prevents too many execute of callback list

---
+++ app/services/resize-updater.js
@@ -0,0 +1,73 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.services')
+        .factory('resizeUpdater', function($window, $q){
+
+            var api = {};
+
+            var size = {
+                width: undefined,
+                height: undefined
+            };
+
+            api.getSize = function(){
+                size.width = $window.innerWidth;
+                size.height = $window.innerHeight;
+                return size;
+            };
+
+            api.list = [];
+
+            api.addCallback = function(callback) {
+
+                api.list[ api.list.length ] = callback;
+
+            };
+
+            var runAll = function() {
+
+                for (var i=0; i<api.list.length; i++){
+                    console.log('('+i+') starting');
+                    var obj = new Obj(api.list[i]);
+                    obj.execute();
+                }
+
+            };
+
+            function Obj(callback) {
+                this.promise = undefined;
+                this.callback = callback;
+                this.execute = function () {
+                    var defer = $q.defer();
+                    this.callback(function (response) {
+                        if (response === 'error') {
+                            defer.reject('Error occured.');
+                        } else {
+                            defer.resolve(response);
+                        }
+                    });
+
+                    this.promise = defer.promise;
+                    this.promise.then(function(result){
+                        console.log('Promise result: _ '+result);
+                    });
+                };
+            }
+
+            angular.element($window).on('resize',(function(){
+                var timer;
+                return function(){
+                    clearTimeout(timer);
+                    timer = setTimeout(function(){
+                        runAll();
+                    }, 500);
+                };
+
+            })());
+
+            return api;
+
+        });
+})();
