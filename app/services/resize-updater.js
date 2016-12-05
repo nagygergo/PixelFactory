c0dfdda84f803361a4c631aa8bc78c109ee94231
Szegedi Dávid <dszegedi@yandex.com>
2016-12-05 15:49:03 +0100

    Add some test cases and listening orientation event
    I put some test cases next to the service.

--- app/services/resize-updater.js
+++ app/services/resize-updater.js
@@ -3,76 +3,87 @@
 
     angular
         .module('app.services')
-        .factory('resizeUpdater', function($window, $q, $timeout, $log){
+        .factory('resizeUpdater', ['$window','$q','$timeout','$log',UpdaterService ]);
 
-            var api = {};
+function UpdaterService($window, $q, $timeout, $log){
 
-            var size = {
-                width: undefined,
-                height: undefined
-            };
+    var api = {};
 
-            api.getSize = function(){
-                size.width = $window.innerWidth;
-                size.height = $window.innerHeight;
-                return size;
-            };
+    var size = {
+        width: undefined,
+        height: undefined,
+        orientation: undefined
+    };
 
-            api.list = [];
 
-            api.addCallback = function(callback) {
-                api.list.push(callback);
-            };
-
-            api.removeCallback = function(callback){
-                var index = api.list.indexOf(callback);
-                delete api.list[index];
-            };
-
-            var runAll = function() {
-
-                for(var item in api.list){
-                    var obj = new Obj(api.list[item]);
-                    obj.execute();
-                }
-
-            };
-
-            function Obj(callback) {
-                this.promise = undefined;
-                this.callback = callback;
-                this.execute = function () {
-                    var defer = $q.defer();
-                    this.callback(function (response) {
-                        if (response === 'error') {
-                            defer.reject('Error occured.');
-                            $log.error('Failed - '+ callback.name + ' - ' + (new Date).toString());
-                        } else {
-                            defer.resolve(response);
-                        }
-                    });
-
-                    this.promise = defer.promise;
-                    this.promise.then(function(result){
-                        $log.log('Executed - '+ callback.name + ' - ' + (new Date).toString());
-                    });
-                };
+    api.getSize = function(){
+        size.width = $window.innerWidth;
+        size.height = $window.innerHeight;
+        size.orientation = (function(){
+            if (size.width >= size.height){
+                return "landscape";
+            } else {
+                return "portrait";
             }
+        })();
+        return size;
+    };
 
-            angular.element($window).on('resize',(function(){
-                var timer_promise;
-                return function(){
-                    if (timer_promise) {
-                        $timeout.cancel(timer_promise);
-                    }
-                    timer_promise = $timeout(function(){
-                        runAll();
-                    }, 500);
-                };
+    api.list = [];
 
-            })());
+    api.addCallback = function(callback) {
+        api.list.push(callback);
+    };
 
-            return api;
+    api.removeCallback = function(callback){
+        var index = api.list.indexOf(callback);
+        delete api.list[index];
+    };
 
-        });
-})();
+    /*private method*/
+    var runAll = function() {
+
+        for(var item in api.list){
+            var obj = new Obj(api.list[item]);
+            obj.execute();
+        }
+
+    };
+
+    function Obj(callback) {
+        this.promise = undefined;
+        this.callback = callback;
+        this.execute = function () {
+            var defer = $q.defer();
+            this.callback(function (response) {
+                if (response === 'error') {
+                    defer.reject('Error occured.');
+                    $log.error('Failed - '+ callback.name + ' - ' + (new Date).toString());
+                } else {
+                    defer.resolve(response);
+                }
+            });
+
+            this.promise = defer.promise;
+            this.promise.then(function(result){
+                $log.log('Executed - '+ callback.name + ' - ' + (new Date).toString());
+            });
+        };
+    }
+
+    angular.element($window).on('resize orientationchange',(function(){
+        var timer_promise;
+        return function(){
+            if (timer_promise) {
+                $timeout.cancel(timer_promise);
+            }
+            timer_promise = $timeout(function(){
+                runAll();
+            }, 500);
+        };
+
+    })());
+
+    return api;
+
+}})();
---
+++ app/services/test-resize-updater.js
@@ -0,0 +1,72 @@
+describe('Testing Resize-updater service',function(){
+
+    var resizeUpdater;
+    var $window;
+    beforeEach(module('app.services'));
+    beforeEach(inject(function(_resizeUpdater_,_$window_){ //parameter name = service name
+        resizeUpdater = _resizeUpdater_;
+        $window = _$window_;
+    }));
+
+    describe('Size object', function(){
+        it('Size object created', function(){
+            expect(resizeUpdater.getSize()).not.toBeUndefined();
+        });
+        it('width', function(){
+            expect(resizeUpdater.getSize().width).toEqual($window.innerWidth);
+        });
+        it('height', function(){
+            expect(resizeUpdater.getSize().height).toEqual($window.innerHeight);
+        });
+        it('orientation-landscape', function(){
+            $window.innerWidth = 1366;  //ez miért működik - értékadás lehetséges??
+            $window.innerHeight = 768;
+            expect(resizeUpdater.getSize().orientation).toEqual("landscape");
+        });
+        it('orientation-portrait', function(){
+            $window.innerWidth = 768;  //ez miért működik - értékadás lehetséges??
+            $window.innerHeight = 1366;
+            expect(resizeUpdater.getSize().orientation).toEqual("portrait");
+        });
+    });
+
+    describe('Add a Callback and resize event',function(){
+
+        var $timeout
+        var $scope;
+        beforeEach(inject(function(_$timeout_,_$rootScope_){ //parameter name = service name
+            $timeout = _$timeout_;
+            $scope = _$rootScope_.$new();
+        }));
+
+        it('Test with trigger resize event',inject(function(){
+
+            $window.innerWidth = 1368;
+            $window.innerHeight = 768;
+            var spy_func = jasmine.createSpy('spy_func');
+
+            resizeUpdater.addCallback(spy_func);
+
+            $window.innerWidth = 1000;
+            angular.element($window).triggerHandler('resize');
+            $scope.$digest();
+            $timeout.flush();
+            expect(spy_func).toHaveBeenCalled();
+        }));
+
+        it('Test without trigger event',inject(function(){
+
+            $window.innerWidth = 1368;
+            $window.innerHeight = 768;
+            var spy_func = jasmine.createSpy('spy_func');
+
+            resizeUpdater.addCallback(spy_func);
+
+            expect(spy_func).not.toHaveBeenCalled();
+        }));
+
+
+    });
+
+
+});
