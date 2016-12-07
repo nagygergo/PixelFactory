0721e793d059bdfba4ef5f5e31b83303153d5c87
Nagy Gergely <nagy.gergely14@gmail.com>
2016-12-07 01:56:16 +0100

    Update 2016-12-07T00:56:14.805Z

--- app/root/root.component.js
+++ app/root/root.component.js
@@ -79,6 +79,7 @@
                     vm.image = imageData;
                     vm.unapproved = true;
                     vm.featuresEnabled = true;
+                    vm.fileFeaturesEnabled = true;
                 });
             }
         }
